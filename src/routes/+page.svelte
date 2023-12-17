faad196d5c1d51b0684f8f41f4aebb1ff4087ab4
Quinten-14 <raymaekersq@gmail.com>
2023-12-17 19:14:12 +0100

    started styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+    let selectedMode:string;
+
+    const changeMode = (button:any) => {
+    selectedMode = button;
+  };
+
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Welcome to the Makefile generator</h1>
+      <p class="py-6">This web app creates a basic Makefile for your C projects. They are not perfect but definetly a great beginning.</p>
+      <button class="btn btn-primary">Start creating</button>
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-center gap-24 bg-base-200">
+    <button class="btn btn-primary">42 Project</button>
+    <button class="btn-primary btn">Own Project</button>
+</div>
